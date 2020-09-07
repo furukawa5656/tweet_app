8bd434fc75be2da3e8b66c090621378e05a273e2
vagrant <vagrant@localhost.localdomain>
2020-09-07 23:43:43 +0000

    finish I

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  #URLの部分は好きに変更可能です
+  get "/" => "home#top"
+  get "about" => "home#about"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
