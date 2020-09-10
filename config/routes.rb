4ea0dd0a6d6f2632228cc3557337b233cb783894
vagrant <vagrant@localhost.localdomain>
2020-09-10 10:00:51 +0000

    finish VII

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  #userを作ることは＝signup
+  get "users/:id" => "users#show"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  post "users/:id/update" => "users#update"
+  delete "users/:id/destroy" => "users#destroy"
+
   #URLの部分は好きに変更可能です
   get "/" => "home#top"
   get "about" => "home#about"
